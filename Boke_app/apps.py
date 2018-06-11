0f01058bbab17a2be7905263fa3be96c6ad21a06
zbc <zbc>
2018-06-11 16:44:39 +0800

    first commit

---
+++ Boke_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BokeAppConfig(AppConfig):
+    name = 'Boke_app'
---
+++ Boke_app/urls.py
@@ -0,0 +1,5 @@
+# @Time    : 18-6-11 下午2:14
+# @Author  : zhangbochao
+# @Site    : 
+# @File    : urls.py
+# @Software: PyCharm
